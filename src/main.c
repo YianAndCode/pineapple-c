dbe22ae5ee93a672ac9bcd764e6cdf9f8c781c49
Y!an <i@yian.me>
2021-11-07 02:10:10 +0800

    add examples

--- src/main.c
+++ src/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "backend.h"
 
 int main(int argc, char* argv[])
 {
-    printf("Hello PINEAPPLE\n");
+    if (argc != 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    char* filename = argv[1];
+    FILE* fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        printf("Error: Could not open file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    int filesize = ftell(fp);
+    rewind(fp);
+
+    char* source_code = (char*)calloc(sizeof(char), filesize + 1);
+    char* tmp = (char*)calloc(sizeof(char), 1024);
+    while((fgets(tmp, 1024, fp)) != NULL){
+        strcat(source_code, tmp);
+    }
+    fclose(fp);
+    free(tmp);
+
+    Execute(source_code);
+
     return 0;
 }
